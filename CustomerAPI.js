6766145cb960ecfb25dcfed57364017a46d1fb71
ShahabGaroosi <45340751+ShahabGaroosi@users.noreply.github.com>
2019-06-02 16:52:58 +0200

    Add files via upload

--- CustomerAPI.js
+++ CustomerAPI.js
@@ -11,7 +11,7 @@ var express = require('express');
 var app = express();
 
 
-const url = "http://localhost:500/";
+const url = "http://localhost:3000/";
 
 // Your Google Cloud Platform project ID
 const projectId = 'api-project-240511';
@@ -24,11 +24,13 @@ const datastore = new Datastore({
 const kind = 'Customer';
 app.use(bodyParser.json());
 
+var customers;
+
 app
     .get('/Customer', async function (req, res) {
         try {
-            var query = await datastore.createQuery(kind)
-            var [customers] = await datastore.runQuery(query)
+            var query = await datastore.createQuery(kind);
+            [customers] = await datastore.runQuery(query);
             res.send(customers)
             console.log(customers)
         } catch (err) {
@@ -39,7 +41,7 @@ app
     .get('/Customer/:id', async function (req, res) {
         try {
             const customerKey = await datastore.key([kind, req.params.id]);
-            const [customer] = await datastore.get(customerKey);
+            const customer = await datastore.get(customerKey)
             res.send(customer)
             console.log(customer)
         }
@@ -50,17 +52,28 @@ app
     })
     .post('/Customer', async function (req, res) {
         try {
-            await datastore.save(req.body);
+            var customers = [];
+            var i;
+            for (i = 0; i < req.body.length; i++) {
+                customers.push({
+                    key: datastore.key([kind, req.body[i].id]),
+                    data: req.body[i],
+                });
+            }
+            await datastore.save(customers);
             res.send(JSON.stringify(req.body));
-            console.log(req.body);
+            console.log(JSON.stringify(req.body));
         } catch (err) {
             res.send(JSON.stringify(err))
         }
     })
     .post('/Customer/:id', async function (req, res) {
-        console.log(req.body);
         try {
-            await datastore.save(req.body);
+            var customer = {
+                key: datastore.key([kind, req.params.id]),
+                data: req.body,
+            };
+            await datastore.save(customer);
             res.send(JSON.stringify(req.body));
             console.log(req.body);
         } catch (err) {
